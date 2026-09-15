a3426c0f8b0ff10a863d2bff32b74e16696f0ee1
velvetSymphony <abhishek@achandrasekar.com>
2026-09-15 23:59:28 +1000

    remove unneccessary funcs, split stuff

--- main.py
+++ main.py
@@ -2,26 +2,19 @@
 
 import json
 import subprocess
+import logging
 
 # NOTE: If the key binds do not fire off, ensure you have a /etc/keyd/default.conf config file populated.
 # NOTE: I'll probably add a check, cover it in a wrapper script.
 
-
-def extract_windows(event, key):
-    match key:
-        case 'WindowsChanged':
-            return event.get('WindowsChanged').get('windows')
-        case 'WindowOpenedOrChanged':
-            return event.get('WindowOpenedOrChanged').get('window')
+# Set logging stuff, replace prints with it later
+logging.basicConfig()
+logger = logging.getLogger()
 
 
 def init_add_browser_ids(browser_ids, BROWSERS, window_item):
     if window_item['app_id'] in BROWSERS:
         browser_ids.add(window_item['id'])
-
-
-def check_window_id(browser_ids, window_id):
-    return True if window_id in browser_ids else False
 
 
 def apply_browser_keyd_config():
@@ -31,26 +24,25 @@ def apply_browser_keyd_config():
 def reset_global_keyd_config():
     subprocess.run(['keyd', 'bind', 'meta+alt.left = left', 'meta+alt.right = right'])
 
-
-def handle_windows_changed(event, BROWSERS):
-    windows = extract_windows(event, key='WindowsChanged')
+# This function may not be required later on, once this script runs as a daemon on startup
+# Only reason this is required now is cause we invoke the script manually once we have opened windows
+def handle_windows_changed(event, BROWSERS, browser_ids):
+    windows = event.get('WindowsChanged').get('windows')
     for window_item in windows:
         init_add_browser_ids(browser_ids, BROWSERS, window_item)
     print(browser_ids)
 
 
 def handle_window_opened_or_changed(event, BROWSERS, browser_ids):
-    window = extract_windows(event, key='WindowOpenedOrChanged')
-    if window['app_id'] in BROWSERS:
-        browser_ids.add(window['id'])  # if it exists, it exists right?
-        apply_browser_keyd_config()
+    window = event.get('WindowOpenedOrChanged').get('window')
+    init_add_browser_ids(browser_ids, BROWSERS, event)
+    apply_browser_keyd_config()
     print(f'added or changed id: {window["id"]} browser_ids: {browser_ids}')
 
 
 def handle_window_focus_changed(event, browser_ids):
-    window_id = event['WindowFocusChanged'].get('id')
-    does_it_belong = check_window_id(browser_ids, window_id)
-    if does_it_belong:
+    window_id = event['WindowFocusChanged']['id']
+    if window_id in browser_ids:
         print(f'id: {window_id} in browser_ids: {browser_ids}')
         apply_browser_keyd_config()
     else:
@@ -59,7 +51,7 @@ def handle_window_focus_changed(event, browser_ids):
 
 
 def handle_window_closed(event, browser_ids):
-    window_id = event['WindowClosed'].get('id')
+    window_id = event['WindowClosed']['id']
     browser_ids.discard(window_id)
     print(f'removed id: {window_id} from browser_ids: {browser_ids}')
 
@@ -78,7 +70,7 @@ if __name__ == '__main__':
         event = json.loads(line)
 
         if event.get('WindowsChanged'):
-            handle_windows_changed(event, BROWSERS)
+            handle_windows_changed(event, BROWSERS, browser_ids)
 
         if event.get('WindowOpenedOrChanged'):
             handle_window_opened_or_changed(event, BROWSERS, browser_ids)
